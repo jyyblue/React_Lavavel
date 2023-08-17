ad69d5eb0d6dfcc024fd5d5845e03dcf5b73a9d4
jyyblue <wenqiangliu344@gmail.com>
2023-08-16 22:39:34 -0700

    change logo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ## API clear
 php artisan route:clear
+
+# Build for product
+npm run build
+push all public build file.
+Go to CPanel
+git pull
--- resources/js/src/component/Sidebar.tsx
+++ resources/js/src/component/Sidebar.tsx
@@ -65,7 +65,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
         <NavLink to="/">
-          <img src={Logo} alt="Logo" />
+          <img className="mx-auto h-10 w-auto hidden dark:block" src='/assets/image/logo.jpg' alt="Logo" />
         </NavLink>
 
         <button
