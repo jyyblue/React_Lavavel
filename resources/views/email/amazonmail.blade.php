fe7dd914202ba40a17ca718836a1b7375e41de22
jyyblue <wenqiangliu344@gmail.com>
2023-08-30 10:57:38 -0700

    remove 50 top product part from email template

--- resources/views/email/amazonmail.blade.php
+++ resources/views/email/amazonmail.blade.php
@@ -263,12 +263,6 @@
                                                         <tbody>
                                                             <tr>
                                                                 <td style="line-height: 24px; font-size: 16px; width: 100%; border-radius: 24px; margin: 0; padding: 40px;" align="left" bgcolor="#ffffff">
-                                                                    <h3 class="text-center" style="padding-top: 0; padding-bottom: 0; font-weight: 500; vertical-align: baseline; font-size: 28px; line-height: 33.6px; margin: 0;" align="center">
-                                                                        50 Product with Highest Discount
-                                                                    </h3>
-                                                                    <p class="text-center text-muted" style="line-height: 24px; font-size: 16px; color: #718096; width: 100%; margin: 0;" align="center">
-                                                                        Lorem Lorem
-                                                                    </p>
                                                                     <table class="p-2 w-full" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
                                                                         <thead>
                                                                             <tr>
